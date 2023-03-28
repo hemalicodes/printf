b2f278fd59629359bb94f1cfa84292ecbb7f1c57
sobitd <sobitdaniel@gmail.com>
2023-03-28 18:13:01 +0300

    edit get_flags.c to pass tests

--- get_flags.c
+++ get_flags.c
@@ -7,35 +7,23 @@
  */
 int get_flags(const char *format, int *i)
 {
-// Characters that represent each flag
-const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
-// Bit value for each flag, in the same order as FLAGS_CH
-const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
-// Current index in the format string
-int curr_i = *i + 1;
-// Active flags, initialized to zero
+/* - + 0 # ' ' */
+/* 1 2 4 8  16 */
+int j, curr_i;
 int flags = 0;
-// Index for looping through FLAGS_CH and FLAGS_ARR
-int j;
-// Loop through the format string, starting at curr_i
-for (; format[curr_i] != '\0'; curr_i++)
+const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 {
-// Check if the current character is a flag character
 for (j = 0; FLAGS_CH[j] != '\0'; j++)
-{
 if (format[curr_i] == FLAGS_CH[j])
 {
-// If it is, set the corresponding bit in flags
 flags |= FLAGS_ARR[j];
 break;
 }
-}
-// If the current character is not a flag character, exit the loop
 if (FLAGS_CH[j] == 0)
 break;
 }
-// Update i to the current index in the format string
 *i = curr_i - 1;
-// Return the calculated flags value
-return flags;
+return (flags);
 }
