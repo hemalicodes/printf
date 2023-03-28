bf62441f2394f956dad42274cc0f3829a5d08c64
sobitd <sobitdaniel@gmail.com>
2023-03-28 18:22:17 +0300

    edit handle_print.c to pass tests

--- handle_print.c
+++ handle_print.c
@@ -2,20 +2,19 @@
 /**
  * handle_print - Prints an argument based on its type
  * @fmt: Formatted string in which to print the arguments.
- * @ind: Pointer to the current index in the format string.
  * @list: List of arguments to be printed.
+ * @ind: ind.
  * @buffer: Buffer array to handle print.
- * @flags: Active flags for formatting the argument.
- * @width: Width specification for the argument.
- * @precision: Precision specification for the argument.
- * @size: Size specifier for the argument.
- *
- * Return: The number of characters printed, or -1 if an error occurred.
+ * @flags: Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: 1 or 2;
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 int flags, int width, int precision, int size)
 {
-int i, unknown_len = 0, printed_chars = -1;
+int i, unknow_len = 0, printed_chars = -1;
 fmt_t fmt_types[] = {
 {'c', print_char}, {'s', print_string}, {'%', print_percent},
 {'i', print_int}, {'d', print_int}, {'b', print_binary},
@@ -24,37 +23,26 @@ fmt_t fmt_types[] = {
 {'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 };
 for (i = 0; fmt_types[i].fmt != '\0'; i++)
-{
 if (fmt[*ind] == fmt_types[i].fmt)
-{
-return fmt_types[i].fn(list, buffer, flags, width, precision, size);
-}
-}
+return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 if (fmt_types[i].fmt == '\0')
 {
 if (fmt[*ind] == '\0')
-{
-return -1;
-}
-unknown_len += write(1, "%%", 1);
+return (-1);
+unknow_len += write(1, "%%", 1);
 if (fmt[*ind - 1] == ' ')
-{
-unknown_len += write(1, " ", 1);
-}
+unknow_len += write(1, " ", 1);
 else if (width)
 {
+--(*ind);
 while (fmt[*ind] != ' ' && fmt[*ind] != '%')
-{
-(*ind)--;
-}
+--(*ind);
 if (fmt[*ind] == ' ')
-{
-(*ind)--;
+--(*ind);
+return (1);
 }
-return 1;
+unknow_len += write(1, &fmt[*ind], 1);
+return (unknow_len);
 }
-unknown_len += write(1, &fmt[*ind], 1);
-return unknown_len;
-}
-return printed_chars;
+return (printed_chars);
 }
